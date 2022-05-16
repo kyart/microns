4212920d10c603cf480de827b90d020e3cdb912b
kyart <kaoru.omiya@gmail.com>
2022-05-16 22:10:25 +0900

    added pages, especially for rinsho.or.jp

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about-page ">
+    <h1 class="title">Aboutページのタイトル</h1>
+    <p>木曾路はすべて山の中である。あるところは岨づたいに行く崖の道であり、あるところは数十間の深さに臨む木曾川の岸であり、あるところは山の尾をめぐる谷の入り口である。一筋の街道はこの深い森林地帯を貫いていた。</p>
+    <p>This option injects dns-prefetch into the head of your project.</p>
+    <p>新規に新規にとできた道はだんだん谷の下の方の位置へと降って来た。</p>
+    <p>道の狭いところには、木を伐って並べ、藤づるでからめ、それで街道の狭いのを補った。長い間にこの木曾路に起こって来た変化は、いくらかずつでも嶮岨な山坂の多いところを歩きよくした。そのかわり、大雨ごとにやって来る河水の氾濫が旅行を困難</p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  font-family: 'Open Sans', sans-serif;
+  text-align: center;
+  padding: 30px;
+}
+</style>
---
+++ pages/rinsho/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="block">
+
+    <img src="https://res.cloudinary.com/kyart2036/image/upload/v1652706178/header_rinsho_wv90oe.jpg"/>
+
+    <h1 style="color:#66eaff;font-size:28px;text-align: center">ご確認ありがとうございました。</h1>
+    <p style="padding:10px;text-align:left;font-size:18px;">メールによる確認にご協力いただきありがとうございました。<br/>当日お待ちしております。</p>
+    <img src="https://res.cloudinary.com/kyart2036/image/upload/v1652706146/nurse2_hzat1e.gif" style="width:600px;" />
+    <p>
+      お問い合わせ<br/>
+      墨田病院　ボランティア受付<br/>
+      フリーダイヤル0120-248-955　<br/>
+      受付：平日（月～金）9：00～17:00　
+    </p>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
